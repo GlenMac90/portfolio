ae05c6de4477a4966c815857d46452f1e44e0945
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-09-20 22:56:38 +0300

    worked on error component

---
+++ app/test/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ErrorGraphic from "../components/contact-page-components/ErrorGraphic";
+
+const test = () => {
+  return (
+    <div className="flex h-screen w-screen items-center">
+      <ErrorGraphic />
+    </div>
+  );
+};
+
+export default test;
