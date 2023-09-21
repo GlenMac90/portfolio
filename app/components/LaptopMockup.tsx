aa2233c01a1577a6ff1e2ce60aa18860b555d599
Glen McCallum <glenmccallum@Glens-MacBook-Pro.local>
2023-09-21 22:06:06 +0300

    small change on laptop mockup

--- app/components/LaptopMockup.tsx
+++ app/components/LaptopMockup.tsx
@@ -8,7 +8,7 @@ const laptopMockupStyles = {
   homepageParent: "h-40 w-64 lg:h-[21rem] lg:w-[35rem]",
   otherPageParent: "h-[10.6rem] w-[17.9rem] sm:h-[19.5rem] sm:w-[32.8rem]",
   homepageChild:
-    "top-[0.18rem] h-[9.22rem] rounded w-[13.26rem] left-[1.432rem] lg:left-12 lg:h-[19rem] lg:w-[28.9rem] lg:rounded-t-[10px] lg:top-[0.4rem]",
+    "top-[0.18rem] h-[9.22rem] rounded w-[13.26rem] left-[1.432rem] lg:left-12 lg:h-[19.25rem] lg:w-[28.9rem] lg:rounded-t-[10px] lg:top-[0.4rem]",
   otherPageChild:
     "top-[0.24rem] h-[9.66rem] rounded left-[1.61rem] sm:top-[0.34rem] w-[14.8rem] sm:left-[2.92rem] sm:h-[18rem] sm:w-[27.2rem] sm:rounded-t-lg",
 };
